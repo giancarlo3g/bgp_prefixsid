0c83c6ac98d3dc6a44221c3e5970b3cb3b6698cd
Giancarlo Garcia <giancarlo3g@gmail.com>
2024-04-22 19:48:22 +0000

    first commit

---
+++ sros-config-backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+yes | cp ./clab-bgpprefixsid/R01-IXR/tftpboot/config.txt ./config/R01-IXR.cfg
+yes | cp ./clab-bgpprefixsid/R02-IXR/tftpboot/config.txt ./config/R02-IXR.cfg
+yes | cp ./clab-bgpprefixsid/R03-IXR/tftpboot/config.txt ./config/R03-IXR.cfg
+yes | cp ./clab-bgpprefixsid/R04-IXR/tftpboot/config.txt ./config/R04-IXR.cfg
+yes | cp ./clab-bgpprefixsid/R05-SR/tftpboot/config.txt ./config/R05-SR.cfg
+yes | cp ./clab-bgpprefixsid/R06-SR/tftpboot/config.txt ./config/R06-SR.cfg
+yes | cp ./clab-bgpprefixsid/R07-SR/tftpboot/config.txt ./config/R07-SR.cfg
+yes | cp ./clab-bgpprefixsid/R08-SR/tftpboot/config.txt ./config/R08-SR.cfg
+yes | cp ./clab-bgpprefixsid/R09-SR/tftpboot/config.txt ./config/R09-SR.cfg
+yes | cp ./clab-bgpprefixsid/R10-SR/tftpboot/config.txt ./config/R10-SR.cfg
+yes | cp ./clab-bgpprefixsid/R11-SR/tftpboot/config.txt ./config/R11-SR.cfg
+yes | cp ./clab-bgpprefixsid/R12-SR/tftpboot/config.txt ./config/R12-SR.cfg
+yes | cp ./clab-bgpprefixsid/R13-IXR/tftpboot/config.txt ./config/R13-IXR.cfg
+yes | cp ./clab-bgpprefixsid/R14-IXR/tftpboot/config.txt ./config/R14-IXR.cfg
+yes | cp ./clab-bgpprefixsid/R15-IXR/tftpboot/config.txt ./config/R15-IXR.cfg
+yes | cp ./clab-bgpprefixsid/R16-IXR/tftpboot/config.txt ./config/R16-IXR.cfg
